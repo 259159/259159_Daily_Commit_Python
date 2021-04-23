8a922588e8799aba91f25190ce22b0787e602cf8
259159 <surakshass976@gmail.com>
2021-04-23 10:11:17 +0530

    programs

---
+++ numer_type_check.py
@@ -0,0 +1,13 @@
+def check(input):
+   if int(input):
+       print("complex number")
+   elif int(input):
+       print("integer")
+   elif float(input):
+       print("float")
+   else:
+       print("String")
+
+n = input("Enter number ")
+check(n)
+

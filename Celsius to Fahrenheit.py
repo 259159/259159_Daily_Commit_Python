949174882f3608f237a0f37920b5c99c4bdd92ba
259159 <surakshass976@gmail.com>
2021-04-25 00:39:41 +0530

    Celsius to Fahrenheit Converter

---
+++ Celsius to Fahrenheit.py
@@ -0,0 +1,10 @@
+celsius = int(input("input a celsius value which need to convert to fahrenheit"))
+
+
+def conv(c):
+    # your code goes here
+    return c * 1.8 + 32
+
+
+fahrenheit = conv(celsius)
+print(fahrenheit)
